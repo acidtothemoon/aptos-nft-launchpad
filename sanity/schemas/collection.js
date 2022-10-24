aab1406f53fd168b761ef4bd64df57e22c2c4e9a
GregShen <gregshen0925@gmail.com>
2022-10-24 17:52:05 +0800

    add schema

---
+++ sanity/schemas/collection.js
@@ -0,0 +1,61 @@
+export default {
+    name: 'collection',
+    title: 'Collection',
+    type: 'document',
+    fields: [
+        {
+            name: 'title',
+            description: 'Enter the title of the NFT Drop',
+            title: 'Title',
+            type: 'string',
+        },
+        {
+            name: 'description',
+            title: 'Description',
+            type: 'string',
+        },
+        {
+            name: 'nftCollectionName',
+            title: 'Name of NFT Collection',
+            type: 'string',
+        },
+        {
+            name: 'address',
+            title: 'Address',
+            type: 'string',
+        },
+        {
+            name: 'slug',
+            title: 'Slug',
+            type: 'slug',
+            options: {
+                source: 'name',
+                maxLength: 96
+            },
+        },
+        {
+            name: 'creator',
+            title: 'Creator',
+            type: 'reference',
+            to: {
+                type: 'creator',
+            }
+        },
+        {
+            name: 'mainImage',
+            title: 'Main Image',
+            type: 'image',
+            options: {
+                hotspot: true
+            }
+        },
+        {
+            name: 'previewImage',
+            title: 'Preview Image',
+            type: 'image',
+            options: {
+                hotspot: true
+            }
+        },
+    ]
+}
